1ca17cccc610423b9dd3449c8cb287fe011cbc47
Gonzalo Sierra <gonzalo.sierra@arcelormittal.com.ar>
2021-09-30 14:05:27 -0300

    forth change

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('mini-css-extract-plugin');
 const TerserPlugin =  require('terser-webpack-plugin');
-const { CleanWebPackPlugin } = require('clean-webpack-plugin').CleanWebpackPlugin;
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -53,7 +53,7 @@ module.exports = {
     new MiniCssExtractPlugin({
         filename: '[name].css'
     }),
-    new CleanWebPackPlugin()
+    new CleanWebpackPlugin(),
   ],
   optimization: {
     minimize: true,
